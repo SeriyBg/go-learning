b5819b9b88152fbfb7b5311f726eefc511ce24ba
Serge Bishyr <seriybg@gmail.com>
2017-06-28 18:32:59 +0300

    Initial commit

---
+++ fs.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+	"net/http"
+	"flag"
+)
+
+func main() {
+	var dir string
+	port := flag.String("port", "3000", "Port to start HTTP on.")
+	path := flag.String("path", "", "Path to server")
+	flag.Parse()
+
+	if *path == "" {
+		dir, _ = os.Getwd()
+	} else {
+		dir = *path
+	}
+
+	http.ListenAndServe(":" + *port, http.FileServer(http.Dir(dir)))
+}
